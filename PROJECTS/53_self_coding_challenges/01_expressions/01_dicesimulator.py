2a09572a09927d35b1639197133e689f6b3bf4cf
kashfshameen <77190678+kashfshameen@users.noreply.github.com>
2024-11-14 18:56:10 +0500

    commit

--- PROJECTS/53_self_coding_challenges/01_expressions/01_dicesimulator.py
+++ PROJECTS/53_self_coding_challenges/01_expressions/01_dicesimulator.py
@@ -1,1 +1,22 @@
 # Simulate rolling two dice, three times. Prints the results of each die roll. This program is used to show how variable scope works
+import random
+
+# Number of sides on each die to roll
+NUM_SIDES = 6
+def roll_dice():
+     die1: int = random.randint(1, NUM_SIDES)
+     die2: int = random.randint(1, NUM_SIDES)
+     print("Die 1:", die1)
+     print("Die 2:", die2)
+     total=die1+die2
+     print("Total of two dice:", total)
+
+def main():
+    die1: int = 10
+    print("die1 in main() starts as: " + str(die1))
+    roll_dice()
+    roll_dice()
+    roll_dice()
+    print("die1 in main() is: " + str(die1))
+
+main()
